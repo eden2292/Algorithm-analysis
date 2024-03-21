8b5a3b848042a7fe0b1ae896004d31585264b305
Eden Peacock <KP798@student.aru.ac.uk>
2024-03-21 09:34:15 +0000

    finish

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System.Diagnostics;
-using System.Timers;
 
+//Method for bubble sort
 static void bubbleSort(int[] arr)
 {
     int n = arr.Length;
+
+    //nested loops to iterate through the array
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - 1; j++)
         {
-            if (arr[j] > arr[j+1])
+            //comparison of elements
+            if (arr[j] > arr[j + 1])
             {
+                //swap elements if needed, using a temporary variable
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -18,58 +22,71 @@ static void bubbleSort(int[] arr)
     }
 }
 
+// method for merge sort
 static void mergeSort(int[] arr, int left, int right)
 {
-    if (left<right)
+    //check if the array contains more than one element
+    if (left < right)
     {
+        //find the middle element
         int middle = left + (right - left) / 2;
 
+        //recursively call merge sort on each half of the array to divide it further
         mergeSort(arr, left, middle);
         mergeSort(arr, middle + 1, right);
 
+        //merge the halves once they have been sorted.
         merge(arr, left, middle, right);
     }
 }
 
+// method for merge.
 static void merge(int[] arr, int left, int middle, int right)
 {
+    //find sizes of subarrays
     int n = middle - left + 1;
     int m = right - middle;
 
+    //create temporary arrays
     int[] leftTemp = new int[n];
     int[] rightTemp = new int[m];
 
+    //counters
     int i;
     int j;
 
-    for(i = 0; i<n; i++)
+    //copy elements into the temporary arrays
+    for (i = 0; i < n; i++)
     {
         leftTemp[i] = arr[left + i];
     }
 
-    for(j = 0; j<m; j++)
+    for (j = 0; j < m; j++)
     {
         rightTemp[j] = arr[middle + 1 + j];
     }
 
+    //reset counters
     i = 0;
     j = 0;
 
     int k = left;
 
-    while(i < n && j < m)
+    //merge the temporary arrays back into the original array.
+    while (i < n && j < m)
     {
-        if( leftTemp[i] <= rightTemp[j])
+        if (leftTemp[i] <= rightTemp[j])
         {
             arr[k++] = leftTemp[i++];
         }
         else
         {
-            arr[k++] = leftTemp[i++]; 
+            arr[k++] = leftTemp[i++];
         }
     }
 
-    while(i < n)
+    //add remaining elements if there are any
+    while (i < n)
     {
         arr[k++] = leftTemp[i++];
     }
@@ -80,137 +97,96 @@ static void merge(int[] arr, int left, int middle, int right)
     }
 }
 
+//method to print array elements.
 static void print(int[] a)
 {
-    foreach(int num in a)
+    foreach (int num in a)
     {
-        Console.WriteLine(num + " " );
+        Console.Write(num + " ");
     }
+    Console.WriteLine();
 }
 
+//method to calculate amount of time taken in milliseconds
 static void timeCalc(Stopwatch stopwatch)
 {
+    //get number of ticks elapsed while algorithm is running
     long ticks = stopwatch.ElapsedTicks;
+    //find the frequency of ticks (this differs from pc to pc)
     long frequency = Stopwatch.Frequency;
 
+    //calculate time in milliseconds
     double timeTaken = ((double)ticks / frequency) * 1000;
 
+    //print the time taken
     Console.WriteLine("Time taken = " + timeTaken);
-
 }
 
+// method to run bubble sort and measure time taken.
+static void runBubble(int[] arr, Stopwatch stopwatch)
+{
+    stopwatch.Start();
+    bubbleSort(arr);
+    stopwatch.Stop();
+    Console.WriteLine("bubble sort");
+    timeCalc(stopwatch);
+    stopwatch.Reset();
+}
+
+//method to run merge sort and measure time taken.
+static void runMerge(int[] arr, Stopwatch stopwatch)
+{
+    stopwatch.Start();
+    mergeSort(arr, 0, arr.Length - 1);
+    stopwatch.Stop();
+    Console.WriteLine("merge sort");
+    timeCalc(stopwatch);
+    stopwatch.Reset();
+}
+
+//create random number generator for array creation
 Random rand = new Random();
+//create stopwatch for timing.
+Stopwatch stopwatch = new Stopwatch();
 
+//initialise array
+//increase number to change number of array elements.
 int[] array = new int[10];
-
-for(int i = 0; i < array.Length; i++)
+//populate array with random numbers
+for (int i = 0; i < array.Length; i++)
 {
     array[i] = rand.Next(0, 100000);
 }
+
+//copy original array to temporary array to ensure algorithms always run on the same data set
 int[] arr = new int[array.Length];
 Array.Copy(array, arr, array.Length);
 
 Console.WriteLine("unsorted array:");
 print(array);
 
-stopwatch.Start();
-bubbleSort(arr);
-stopwatch.Stop();
-Console.WriteLine("bubble sort 1");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
-//print(array);
-
+//run bubble sort multiple times
+runBubble(arr, stopwatch);
+//reset temporary array to original order
+Array.Copy(array, arr, array.Length);
+runBubble(arr, stopwatch);
+Array.Copy(array, arr, array.Length);
+runBubble(arr, stopwatch);
+Array.Copy(array, arr, array.Length);
+runBubble(arr, stopwatch);
+Array.Copy(array, arr, array.Length);
+runBubble(arr, stopwatch);
 Array.Copy(array, arr, array.Length);
 
-stopwatch.Start();
-bubbleSort(arr);
-stopwatch.Stop();
-Console.WriteLine("bubble sort 2");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
+//run merge sort multiple times
+runMerge(arr, stopwatch);
 Array.Copy(array, arr, array.Length);
-
-stopwatch.Start();
-bubbleSort(arr);
-stopwatch.Stop();
-Console.WriteLine("bubble sort 3");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
+runMerge(arr, stopwatch);
 Array.Copy(array, arr, array.Length);
-
-stopwatch.Start();
-bubbleSort(arr);
-stopwatch.Stop();
-Console.WriteLine("bubble sort 4");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
+runMerge(arr, stopwatch);
 Array.Copy(array, arr, array.Length);
-
-stopwatch.Start();
-bubbleSort(arr);
-stopwatch.Stop();
-Console.WriteLine("bubble sort 5");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
+runMerge(arr, stopwatch);
 Array.Copy(array, arr, array.Length);
+runMerge(arr, stopwatch);
 
-stopwatch.Start();
-mergeSort(arr, 0, arr.Length - 1);
-stopwatch.Stop();
-Console.WriteLine("merge sort 1");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
-//print(array);
-
-Array.Copy(array, arr, array.Length);
-
-stopwatch.Start();
-mergeSort(arr, 0, arr.Length - 1);
-stopwatch.Stop();
-Console.WriteLine("merge sort 2");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
-Array.Copy(array, arr, array.Length);
-
-stopwatch.Start();
-mergeSort(arr, 0, arr.Length - 1);
-stopwatch.Stop();
-Console.WriteLine("merge sort 3");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
-Array.Copy(array, arr, array.Length);
-
-stopwatch.Start();
-mergeSort(arr, 0, arr.Length - 1);
-stopwatch.Stop();
-Console.WriteLine("merge sort 4");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
-
-Array.Copy(array, arr, array.Length);
-
-stopwatch.Start();
-mergeSort(arr, 0, arr.Length - 1);
-stopwatch.Stop();
-Console.WriteLine("merge sort 5");
-//print(arr);
-timeCalc(stopwatch);
-stopwatch.Reset();
+//algorithms are run multiple times to get an average.
